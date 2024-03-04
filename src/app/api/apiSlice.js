1aaafbc8969033c0a6b4f5fe56a2dbd18074e41a
Martin Garza <54215833+MartinGarzaFCFM@users.noreply.github.com>
2024-03-04 15:39:22 -0600

    Update apiSlice.js
    Cambiado para conectarse al API en render

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://artifacts-api-awgx.onrender.com/',
+    baseUrl: 'https://artifacts-api-jlx7.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
